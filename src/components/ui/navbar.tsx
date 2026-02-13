41f38781c22c935c4349ed0d911b48543e933687
Pranay_Prat <pranay.pratap15@gmail.com>
2026-02-14 05:12:24 +0530

    fixed about

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -29,7 +29,7 @@ const Navbar = () => {
   }
 
   const navItems = [
-    { name: "About", href: "#about", type: "a" },
+    { name: "About", href: "/#about", type: "link" },
     { name: "Lineups", href: "/lineups/all", type: "link" }
   ]
 
